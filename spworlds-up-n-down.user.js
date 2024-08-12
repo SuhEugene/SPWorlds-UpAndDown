5fafce5dca665d9f1781571cac9987df3fb7c7d4
SuhEugene <suheugene@ya.ru>
2024-08-12 13:42:16 +0300

    Update the whole script
    * Скрипт теперь работает не только на sp, но и на всех остальных проектах
    
    * Апвоуты и даунвоуты отображаются в кнопке, а если её найти не удалось - только тогда под заголовком
    
    * Обрабатываются ещё и POST запросы, чтобы обновить счётчик при нажатии на апвоут/даунвоут
    
    * Обновлён манифес скрипта. Описание попроще, имя покрасивше, добавил себя в авторы и разрешил все пути spworlds.ru

--- spworlds-up-n-down.user.js
+++ spworlds-up-n-down.user.js
@@ -1,99 +1,175 @@
 // ==UserScript==
-// @name         Spworlds SP - Upvotes and Downvotes viewer
+// @name         SPWorlds - Upvotes and Downvotes viewer
 // @namespace    http://tampermonkey.net/
-// @version      2024-06-30
-// @description  Отображает Upvotes и Downvotes поста возле даты публикации поста.
-// @author       DearFox and ChatGPT 4o
-// @match        https://spworlds.ru/sp/*
+// @version      2024-08-07
+// @description  Отображает апвоуты и даунвоуты поста, вместо их разницы.
+// @author       SuhEugene, DearFox and ChatGPT 4o
+// @match        https://spworlds.ru/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=spworlds.ru
 // @grant        none
 // ==/UserScript==
 
 (function () {
   'use strict';
+  // Сохраняем оригинальный для вызова в будущем
+  const originalFetch = fetch;
+
+  const DIVIDER =
+    '<p style="background:#3d3d4b; width:2px; display:inline-block; height: 20px; border-radius:2px; margin: 0 12px;"></p>';
+
   // Функция для обработки данных поста
   function processPostData(data) {
-    if (data && data.id && data.upvotes !== undefined && data.downvotes !== undefined) {
-      const id = data.id;
-      const upvotes = data.upvotes;
-      const downvotes = data.downvotes;
-      console.log(`ID: ${id}, Upvotes: ${upvotes}, Downvotes: ${downvotes}`);
+    if (!data) return console.warn('Переданный ответ пуст');
 
-      // Флаг для отслеживания обновления элемента
-      let isUpdated = false;
+    if (!data.id) return console.warn('Переданный ответ не содержит идентификатор поста');
 
-      // Функция для обновления элемента <a>
-      const updateAnchor = () => {
-        const anchor = document.querySelector(`a[href="/sp/feed/${id}"]`);
-        if (anchor && !isUpdated) {
-          anchor.innerHTML += ` : + ${upvotes} | - ${downvotes}`;
-          isUpdated = true; // Обновляем флаг после успешного обновления
-        }
-      };
+    if (data.upvotes === undefined || data.downvotes === undefined)
+      return console.warn('Ответ не содержит полей голосов за/против поста');
 
-      // Создание MutationObserver для отслеживания изменений в DOM
-      const observer = new MutationObserver(() => {
-        updateAnchor();
-        if (isUpdated) {
-          observer.disconnect(); // Останавливаем наблюдение, как только элемент найден и обновлен
-        }
-      });
+    const id = data.id;
+    const upvotes = data.upvotes;
+    const downvotes = data.downvotes;
+    console.log(`ID: ${id}, Upvotes: ${upvotes}, Downvotes: ${downvotes}`);
 
-      // Начало наблюдения за изменениями в DOM
-      observer.observe(document.body, { childList: true, subtree: true });
+    const updateAnchor = anchor => {
+      console.warn('Заменяем ссылку...');
+      const originalText = anchor.innerText.split(' : + ')[0];
+      anchor.innerText = `${originalText} : + ${upvotes} | - ${downvotes}`;
+    };
 
-      // Пытаемся обновить элемент сразу, если он уже существует
-      updateAnchor();
-    } else {
-      console.error('Ответ не содержит нужные поля.');
-    }
+    // Функция для обновления элемента
+    const updatePost = () => {
+      const anchor = document.querySelector(`a[href="/sp/feed/${id}"]`);
+      if (!anchor) return false;
+
+      const postEl = anchor.closest('.relative.space-y-4');
+      if (!postEl) {
+        console.warn('Пост не найден');
+        updateAnchor(anchor);
+        return true;
+      }
+      const counterEl = postEl.querySelector('button[title=Апвоут] + p');
+      if (!counterEl) {
+        console.warn('Счётчик не найден');
+        updateAnchor(anchor);
+        return true;
+      }
+      const upvoteEl = postEl.querySelector('button[title=Апвоут]');
+      const downvoteEl = postEl.querySelector('button[title=Даунвоут]');
+      if (!upvoteEl || !downvoteEl) {
+        console.warn('Одна из кнопок не найдена');
+        updateAnchor(anchor);
+        return true;
+      }
+      const counterParent = upvoteEl.parentElement;
+      if (!counterParent) {
+        console.warn('Не найден родитель кнопок');
+        updateAnchor(anchor);
+        return true;
+      }
+
+      const upvoteSVGEl = upvoteEl.querySelector('svg');
+      const downvoteSVGEl = downvoteEl.querySelector('svg');
+      if (!upvoteSVGEl || !downvoteSVGEl) {
+        console.warn('SVG одной из кнопок не найден');
+        updateAnchor(anchor);
+        return true;
+      }
+
+      upvoteEl.classList.add('flex');
+      upvoteEl.classList.add('font-medium');
+      downvoteEl.classList.add('flex');
+      downvoteEl.classList.add('font-medium');
+      upvoteEl.innerHTML = `${upvoteSVGEl.outerHTML}<span style="color: white;">${upvotes}</span>`;
+      downvoteEl.innerHTML = `<span style="color: white;">${downvotes}</span>${downvoteSVGEl.outerHTML}`;
+
+      counterEl.outerHTML = DIVIDER;
+
+      return true;
+    };
+
+    // Создание MutationObserver для отслеживания изменений в DOM
+    const observer = new MutationObserver(() => updatePost() && observer.disconnect());
+
+    // Начало наблюдения за изменениями в DOM
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    // Пытаемся обновить элемент сразу, если он уже существует
+    updatePost();
   }
 
+  const GET_METHOD_REGEXES = [
+    // Конкретный пост
+    /^https:\/\/spworlds\.ru\/api\/[a-z0-9_]+\/posts\/[0-9a-fA-F-]+$/,
+
+    // Посты аккаунта
+    /^https:\/\/spworlds\.ru\/api\/[a-z0-9_]+\/posts\/from\/account\/[0-9a-fA-F-]+(\?.*)?$/,
+
+    // Посты группы
+    /^https:\/\/spworlds\.ru\/api\/[a-z0-9_]+\/posts\/from\/group\/[0-9a-fA-F-]+(\?.*)?$/,
+
+    // Посты страницы новостей
+    /^https:\/\/spworlds\.ru\/api\/[a-z0-9_]+\/posts\?.*$/
+  ];
+
+  const checkGetRequest = async (url, options, response) => {
+    if (!GET_METHOD_REGEXES.some(regex => regex.test(url))) return;
+
+    try {
+      const data = await response.json();
+      if (Array.isArray(data)) data.forEach(post => processPostData(post));
+      else processPostData(data);
+    } catch (error) {
+      console.error('Ошибка при разборе ответа на получение поста(ов):', error);
+    }
+  };
+
+  const UPDATE_POST_REGEX = /^https:\/\/spworlds\.ru\/api\/[a-z0-9_]+\/posts\/[0-9a-fA-F-]+$/;
+
+  const checkPostRequest = async (url, options, response) => {
+    const match = url.match(UPDATE_POST_REGEX);
+    if (!match) return;
+
+    const [serverUrl] = match;
+
+    const body = options?.body;
+    if (!body) return;
+
+    let jsonBody = null;
+    try {
+      jsonBody = JSON.parse(body);
+    } catch (error) {
+      console.error('Не удалось пропарсить тело POST запроса к API sp worlds', error);
+    }
+    if (!jsonBody) return;
+
+    if (jsonBody.isUpvote === undefined || jsonBody.vote === undefined) return;
+    try {
+      const json = await originalFetch(serverUrl, {
+        credentials: 'include'
+      }).then(r => r.json());
+      if (!json) return;
+      processPostData(json);
+    } catch (error) {
+      console.error('Ошибка кастомного получения поста:', error);
+    }
+  };
+
   // Перехват fetch
-  const originalFetch = fetch;
-  window.fetch = async function () {
-    const url = arguments[0];
-    const options = arguments[1] || {};
-    // Проверяем метод запроса
-    const method = options.method || 'GET';
+  window.fetch = async function (url, options = {}) {
+    const method = (options.method || 'GET').toUpperCase();
 
-    // Регулярные выражения для проверки URL
-    const regexPost = /^https:\/\/spworlds\.ru\/api\/sp\/posts\/[0-9a-fA-F-]+$/;
-    const regexAccount =
-      /^https:\/\/spworlds\.ru\/api\/sp\/posts\/from\/account\/[0-9a-fA-F-]+(\?.*)?$/;
-    const regexSort =
-      /^https:\/\/spworlds\.ru\/api\/sp\/posts\?sort=new&source=\w+&p=\d+&time=\d+$/;
-
-    if (
-      method.toUpperCase() === 'GET' &&
-      (regexPost.test(url) || regexAccount.test(url) || regexSort.test(url))
-    ) {
+    if (method === 'GET') {
       const response = await originalFetch.apply(this, arguments);
-      const clonedResponse = response.clone();
-
-      // Обработка ответа
-      clonedResponse
-        .json()
-        .then(data => {
-          if (regexPost.test(url)) {
-            // Обработка одиночного поста
-            processPostData(data);
-          } else if ((regexAccount.test(url) || regexSort.test(url)) && Array.isArray(data)) {
-            // Обработка массива постов
-            data.forEach(post => processPostData(post));
-          } else {
-            console.error('Ответ не соответствует ожидаемому формату.');
-          }
-        })
-        .catch(error => {
-          console.error('Ошибка при разборе ответа:', error);
-        });
-
+      checkGetRequest(url, options, response.clone());
+      return response;
+    } else if (method === 'POST') {
+      const response = await originalFetch.apply(this, arguments);
+      checkPostRequest(url, options, response.clone());
       return response;
     }
 
     // Если запрос не соответствует условиям, выполняем его без изменений
     return originalFetch.apply(this, arguments);
   };
-  // Your code here...
 })();
